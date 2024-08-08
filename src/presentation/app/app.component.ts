0fc914c8f91c2d4e69a2df3d5a9834bfe8d2faf9
Malcolm453 <malcolm@vetro.co.za>
2024-08-08 11:36:06 +0200

    improving the structure of the project

--- src/presentation/app/app.component.ts
+++ src/presentation/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { ContainerComponent } from "./container/container.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
-import { Top5pollsComponent } from "./top5polls/top5polls.component";
-import { Top5pollresultsComponent } from "./top5pollresults/top5pollresults.component";
-import { HeroComponent } from "./hero/hero.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { Top5pollsComponent } from "./shared/components/top5polls/top5polls.component";
+import { Top5pollresultsComponent } from "./shared/components/top5pollresults/top5pollresults.component";
+import { HeroComponent } from "./shared/components/hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, ContainerComponent, FooterComponent, HeaderComponent, Top5pollsComponent, Top5pollresultsComponent, HeroComponent],
+  imports: [RouterOutlet, LoginComponent, FooterComponent, HeaderComponent, Top5pollsComponent, Top5pollresultsComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
